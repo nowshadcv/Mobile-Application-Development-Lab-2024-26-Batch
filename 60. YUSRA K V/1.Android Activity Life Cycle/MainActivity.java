e397c81dfba02cee90ea0f44e876913110124659
YUSRA <yusraash44@gmail.com>
2025-08-06 15:18:36 +0530

    Add files via upload

---
+++ 60. YUSRA K V/1.Android Activity Life Cycle/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.alc2025;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle", "onCreate invoked");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle", "onStart invoked");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle", "onResume invoked");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle", "onPause invoked");
+    }
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle", "onStop invoked");
+    }
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle", "onRestart invoked");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle", "onDestroy invoked");
+    }
+}
