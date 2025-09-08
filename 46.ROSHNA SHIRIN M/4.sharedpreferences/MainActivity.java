1544c44ff4f098792de7092bbdd958b7d3d35855
rosh4976 <roshnashirinm2002@gmail.com>
2025-09-08 14:12:29 +0530

    Create MainActivity.java

---
+++ 46.ROSHNA SHIRIN M/4.sharedpreferences/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,mobilenum,email,pass1,pass2;
+    Button submit;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.username);
+        mobilenum = (EditText) findViewById(R.id.number);
+        email = (EditText) findViewById(R.id.email);
+        pass1 = (EditText) findViewById(R.id.password);
+        pass2 = (EditText) findViewById(R.id.conpassword);
+        submit = (Button) findViewById(R.id.loginbtn);
+        sharedPreferences=getSharedPreferences("UserDetails",MODE_PRIVATE);
+        editor=sharedPreferences.edit();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernames=username.getText().toString().trim();
+                String mobilei=mobilenum.getText().toString().trim();
+                String emails=email.getText().toString().trim();
+                String pass1s=pass1.getText().toString().trim();
+                String pass2s=pass2.getText().toString().trim();
+                if (usernames.isEmpty()){
+                    username.setError("username is empty");
+                    username.requestFocus();
+                    return;
+                }
+                if (mobilei.isEmpty()){
+                    mobilenum.setError("mobilenumber is empty");
+                    mobilenum.requestFocus();
+                    return;
+                }
+                if (emails.isEmpty()){
+                    email.setError("Input Email");
+                    email.requestFocus();
+                    return;
+                }
+                if (pass1s.isEmpty()){
+                    pass1.setError("Enter password");
+                    pass1.requestFocus();
+                    return;
+                }
+                if (pass2s.isEmpty()){
+                    pass2.setError("username is empty");
+                    pass2.requestFocus();
+                    return;
+                }
+                if (pass1s.length()<6){
+                    pass1.setError("length must be minimum6characters");
+                    pass1.requestFocus();
+                    return;
+                }
+                if(!pass1s.equals(pass2s)){
+                    pass2.setError("passwordnot matched");
+                    pass2.requestFocus();
+                    return;
+                }
+                Toast.makeText(MainActivity.this,"Registration successful", Toast.LENGTH_LONG).show();
+                editor.putString("keyusername",usernames);
+                editor.putString("keymobile",mobilei);
+                editor.putString("keyemail",emails);
+                editor.putString("keypassword",pass2s);
+                editor.apply();
+
+
+
+            }
+        });
+
+    }
+}
