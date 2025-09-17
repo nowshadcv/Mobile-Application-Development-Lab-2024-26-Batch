00dda7b3bb6fd521a8f9177f7c83c4e763fde0c7
YUSRA <yusraash44@gmail.com>
2025-09-17 16:26:08 +0530

    Add files via upload

---
+++ 58. SWAFWANA SHERI K P/5.Toggle/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.toggleimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView i1,i2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1=(ImageView)findViewById(R.id.img1);
+        i2=(ImageView)findViewById(R.id.img2);
+        i1.setOnClickListener(this::onClick);
+        i2.setOnClickListener(this::onClick);
+    }
+    public void onClick(View v){
+        if(v.getId()==R.id.img1) {
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            i2.setVisibility(View.GONE);
+            i1.setVisibility(View.VISIBLE);
+        }
+
+    }
+}
