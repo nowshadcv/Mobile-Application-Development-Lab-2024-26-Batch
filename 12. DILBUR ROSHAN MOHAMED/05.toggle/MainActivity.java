afbad64ed1f380ff33303e8d10eeb4dbae34e136
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-09-17 15:10:41 +0530

    Create MainActivity.java

---
+++ 12. DILBUR ROSHAN MOHAMED/05.toggle/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView i1,i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1 = (ImageView) findViewById(R.id.img1);
+        i2 = (ImageView) findViewById(R.id.img2);
+        i1.setOnClickListener(this::OnClick);
+        i2.setOnClickListener(this::OnClick);
+
+    }
+    public void OnClick(View v){
+        if (v.getId()==R.id.img1) {
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            i2.setVisibility(View.GONE);
+            i1.setVisibility(View.VISIBLE);
+        }
+
+
+
+    }
+}
