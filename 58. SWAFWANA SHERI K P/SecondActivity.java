239ebe999f45d712f2c430d2d66370fa6dd1c926
swafwana <swafwanasheri17@gmail.com>
2025-09-29 12:50:37 +0530

    Add files via upload

---
+++ 58. SWAFWANA SHERI K P/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle bundle=getIntent().getExtras();
+        if (bundle != null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+    private void setImage(String imgindex){
+        ImageView imageView= (ImageView) findViewById(R.id.img1);
+        switch (imgindex){
+            case "1":
+                imageView.setImageResource(R.drawable.blueberry);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.cheesecake);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.strawberry);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.pomegranate);
+                break;
+        }
+
+    }
+
+}
