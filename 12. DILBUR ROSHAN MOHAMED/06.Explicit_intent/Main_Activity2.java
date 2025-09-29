e35465516f2dd462aa467bfb5c8a5ce7282f5a7a
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-09-29 11:31:07 +0530

    Create Main_Activity2.java

---
+++ 12. DILBUR ROSHAN MOHAMED/06.Explicit_intent/Main_Activity2.java
@@ -0,0 +1,39 @@
+package com.example.explicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if (bundle!=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+
+        }
+    }
+
+    private void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.img1);
+        switch (index){
+            case "1":
+                imageView.setImageResource(R.drawable.rose);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.car);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.watch);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.phone);
+                break;
+        }
+    }
+}
