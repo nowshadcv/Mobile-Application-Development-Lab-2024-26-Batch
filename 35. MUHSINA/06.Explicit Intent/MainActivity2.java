d9d1bdfef850933eeaacd0aabdd25ed1d0f01f23
muhsina_mujeeb_ <muhsinamuhsina81@gmail.com>
2025-09-29 12:25:40 +0530

    Create MainActivity2.java

---
+++ 35. MUHSINA/06.Explicit Intent/MainActivity2.java
@@ -0,0 +1,45 @@
+
+package com.example.explicitintend;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if (bundle!=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+
+
+    private void setImage(String imgindex){
+        ImageView imageView=(ImageView) findViewById(R.id.img1);
+
+        switch(imgindex){
+            case "1":
+                imageView.setImageResource(R.drawable.india);
+                break;
+
+            case "2":
+                imageView.setImageResource(R.drawable.greece);
+                break;
+
+            case "3":
+                imageView.setImageResource(R.drawable.switz);
+                break;
+
+            case "4":
+                imageView.setImageResource(R.drawable.uk);
+                break;
+
+        }
+    }
+}
