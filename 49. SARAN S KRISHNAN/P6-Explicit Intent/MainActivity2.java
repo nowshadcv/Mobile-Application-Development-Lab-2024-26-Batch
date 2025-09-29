0bfc00ff2919147da6aef636edba87e22a842137
Saran-sk52 <saransk1249@gmail.com>
2025-09-29 11:31:25 +0530

    Update MainActivity2.java

--- 49. SARAN S KRISHNAN/P6-Explicit Intent/MainActivity2.java
+++ 49. SARAN S KRISHNAN/P6-Explicit Intent/MainActivity2.java
@@ -1,1 +1,37 @@
-v
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+    private void setImage(String index){
+        ImageView imageView=(ImageView)findViewById(R.id.img1);
+        switch (index){
+            case "1":
+                imageView.setImageResource(R.drawable.pic1);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.pic2);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.pic3);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.pic4);
+                break;
+        }
+    }
+}
