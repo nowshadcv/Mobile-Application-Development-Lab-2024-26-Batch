381f7d03be691ecd9700b1ec96d25436d054fcfd
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2025-09-29 12:09:02 +0530

    Create s

---
+++ 54. SNEHA K P/6. Explicit_Intents/second_activity.java
@@ -0,0 +1,37 @@
+package com.example.explicit_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+
+    private void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.img1);
+        switch (index){
+            case "1":
+                imageView.setImageResource(R.drawable.dragon_fruit);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.grape);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.pineapple);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.pomegranate);
+        }
+    }
+}
