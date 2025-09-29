cd1c85d387ea0fa027a4aa456cb9309b23eff092
Saran-sk52 <saransk1249@gmail.com>
2025-09-29 11:31:42 +0530

    Update MainActivity.java

--- 49. SARAN S KRISHNAN/P6-Explicit Intent/MainActivity.java
+++ 49. SARAN S KRISHNAN/P6-Explicit Intent/MainActivity.java
@@ -1,1 +1,23 @@
-f
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onIndexSendbuttonClicked(View view) {
+        Intent intent = new Intent(this,MainActivity2.class);
+        EditText editText = (EditText) findViewById(R.id.text);
+        String index = editText.getText().toString();
+        intent.putExtra("imageIndex", index);
+    }
+}
