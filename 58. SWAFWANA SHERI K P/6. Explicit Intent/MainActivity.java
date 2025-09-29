5805baa128cd42ea0fc74688774c62d10e03faa8
swafwana <swafwanasheri17@gmail.com>
2025-09-29 12:53:27 +0530

    Add files via upload

---
+++ 58. SWAFWANA SHERI K P/6. Explicit Intent/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onIndexSendButtonClicked(View v){
+        Intent intent=new Intent(this,SecondActivity.class);
+        EditText editText=(EditText) findViewById(R.id.text);
+        String index= editText.getText().toString();
+        intent.putExtra("ImageIndex",index);
+        startActivity(intent);
+    }
+
+}
