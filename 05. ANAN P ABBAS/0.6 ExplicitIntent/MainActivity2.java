011c6762a64cc0b67ce51532930d1e25285bf3eb
ananpabbas <ananpa2323@gmail.com>
2025-09-29 12:24:23 +0530

    Create MainActivity2.java

---
+++ 05. ANAN P ABBAS/0.6 ExplicitIntent/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+
+        }
+    }
+    private  void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.img1);
+        switch(index){
+            case "1":
+                imageView.setImageResource(R.drawable.apple);
+            break;
+            case "2":
+                imageView.setImageResource(R.drawable.pineapple);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.orange);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.pomegranate);
+                break;
+
+        }
+    }
+}
