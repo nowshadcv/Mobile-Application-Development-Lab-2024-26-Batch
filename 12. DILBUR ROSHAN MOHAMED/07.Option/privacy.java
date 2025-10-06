4a0d2531fe6ef0096cc90b1ce1391afcfa317455
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-10-06 12:17:09 +0530

    Add files via upload

---
+++ 12. DILBUR ROSHAN MOHAMED/07.Option/privacy.java
@@ -0,0 +1,14 @@
+package com.example.option;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class privacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+    }
+}
