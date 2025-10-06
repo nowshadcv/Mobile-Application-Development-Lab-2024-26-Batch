3dc5659967b7e2ecb6df8977cc1ce963a774de12
naadhi-web <naadhifaisal@gmail.com>
2025-10-06 12:19:32 +0530

    Add files via upload

---
+++ 36.NAADHI RAHMAN P/07.Option Menu/Termsandcondition.java
@@ -0,0 +1,14 @@
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Termsandcondition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termsandcondition);
+    }
+}
