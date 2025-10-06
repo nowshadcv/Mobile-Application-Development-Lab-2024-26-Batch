cbec5aa89d22e0fb1aa91b77f9532b8e2dcaf160
ABDUL JANEEB K <janeebkonami@gmail.com>
2025-10-06 12:15:42 +0530

    Add files via upload

---
+++ 01. ABDUL JANEEB K/07. Option Menu/fragment_second.java
@@ -0,0 +1,14 @@
+package com.example.optionmenu2025;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_second);
+    }
+}
