0c8cd0435fa575ac48dc01a4d43c0fd36c376ada
Naheel <96335640+naheelkk@users.noreply.github.com>
2025-10-08 10:52:06 +0000

    LFG

---
+++ 38. NAHEEL K K/08. Array Adapter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+    String fruitlist[] = {"Apple","Banana","Dates","Orange"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textview, fruitlist);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
+                Log.i("ListView", "Item is Clicked @position "+position);
+                if(position == 0){
+                    startActivity(new Intent(MainActivity.this, aple.class));
+                }
+                if(position == 1){
+                    startActivity(new Intent(MainActivity.this, banana.class));
+                }
+                if(position == 2){
+                    startActivity(new Intent(MainActivity.this, dates.class));
+                }
+                if(position == 4){
+                    startActivity(new Intent(MainActivity.this,orange.class ));
+                }
+            }
+        });
+    }
+}
