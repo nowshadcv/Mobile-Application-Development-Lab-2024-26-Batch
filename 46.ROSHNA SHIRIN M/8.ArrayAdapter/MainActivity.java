2487a14594f20880118e8d1575830040d72def3c
rosh4976 <roshnashirinm2002@gmail.com>
2025-10-08 16:35:10 +0530

    Add files via upload

---
+++ 46.ROSHNA SHIRIN M/8.ArrayAdapter/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    String fruitlist[]={"banana","orange","grapes","dates"};
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView) findViewById(R.id.list);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.activity_listview,R.id.textview
+                ,fruitlist);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.i("ListView","item is clicked @ position"+position);
+                if(position==0){
+                    startActivity(new Intent(MainActivity.this, banana.class));
+                }
+                else if (position==1){
+                    startActivity(new Intent(MainActivity.this, orange.class));
+                }
+                else if (position==2){
+                    startActivity(new Intent(MainActivity.this, grapes.class));
+                }
+                else if (position==3){
+                    startActivity(new Intent(MainActivity.this, dates.class));
+                }
+
+            }
+    });
+    }
+}
