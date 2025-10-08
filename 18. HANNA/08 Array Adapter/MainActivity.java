698f601a2fe2360e57b17feb07dc8f5f77665117
HANNA <vhanna659@gmail.com>
2025-10-08 16:08:03 +0530

    Add files via upload

---
+++ 18. HANNA/08 Array Adapter/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.array_adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+public class MainActivity extends AppCompatActivity {
+
+    String fruitList[] = { "Apple", "Banana", "Dates", "Orange", "Pomegranate" };
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.activity_listview,
+                R.id.textview,
+                fruitList
+        );
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.i("ListView", "Item is clicked @position " + position);
+
+                if (position == 0) {
+                    startActivity(new Intent(MainActivity.this, activity_apple.class));
+                } else if (position == 1) {
+                    startActivity(new Intent(MainActivity.this, activity_banana.class));
+                } else if (position == 2) {
+                    startActivity(new Intent(MainActivity.this, activity_dates.class));
+                } else if (position == 3) {
+                    startActivity(new Intent(MainActivity.this, activity_orange.class));
+                } else if (position == 4) {
+                    startActivity(new Intent(MainActivity.this, activity_pomegranate.class));
+                }
+            }
+        });
+    }
+}
