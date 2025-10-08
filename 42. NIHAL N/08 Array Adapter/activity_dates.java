368150ce1b7752ead4c64f2530d75edc2f316f8e
Nihal N <112004307+nihalshx@users.noreply.github.com>
2025-10-08 16:17:15 +0530

    Add files via upload

---
+++ 42. NIHAL N/08 Array Adapter/activity_dates.java
@@ -0,0 +1,14 @@
+package com.example.array_adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_dates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dates);
+    }
+}
