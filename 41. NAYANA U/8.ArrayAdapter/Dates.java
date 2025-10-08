c65f7f69eabccdc2960d331e0e3ae7818007dee6
Nayanastal <9listal@gmail.com>
2025-10-08 16:16:39 +0530

    Add files via upload

---
+++ 41. NAYANA U/8.ArrayAdapter/Dates.java
@@ -0,0 +1,14 @@
+package com.example.array_adaptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dates);
+    }
+}
