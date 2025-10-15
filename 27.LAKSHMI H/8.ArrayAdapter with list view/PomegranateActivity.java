94cc2576c60ac87274f8c071f531e83e720095a1
Lakshmi H <100887312+Lakshmiha@users.noreply.github.com>
2025-10-15 15:55:51 +0530

    Create PomegranateActivity.java

---
+++ 27.LAKSHMI H/8.ArrayAdapter with list view/PomegranateActivity.java
@@ -0,0 +1,14 @@
+package com.example.arrayadapterwithlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PomegranateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pomegranate);
+    }
+}
