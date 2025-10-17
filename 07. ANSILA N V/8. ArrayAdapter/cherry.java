015e21b26003694add50c6297ce4beb086ca3b60
ansilasharaf <ansilanachiveettil@gmail.com>
2025-10-17 18:47:41 +0400

    Create cherry.java

---
+++ 07. ANSILA N V/8. ArrayAdapter/cherry.java
@@ -0,0 +1,15 @@
+
+package com.example.arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cherry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cherry);
+    }
+}
