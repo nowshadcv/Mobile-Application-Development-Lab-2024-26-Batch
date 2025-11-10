26390d867a4f03ec9de9df45910934f75d6d12d6
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-11-10 12:12:11 +0530

    Update MainActivity_2.java

--- 37. NAFEESA K T/6. Explicit Intents/MainActivity_2.java
+++ 37. NAFEESA K T/6. Explicit Intents/MainActivity_2.java
@@ -5,21 +5,39 @@ import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle!=null) {
+            String index = bundle.getString("ImageIndex");
+            if (index != null) {
+                setImage(index);
+            }
+        }
     }
 
-    public void onIndexSendbuttonClicked(View view){
-        Intent intent = new Intent(this,MainActivity_2.class);
-        EditText editText = (EditText) findViewById(R.id.text);
-        String index = editText.getText().toString();
-        intent.putExtra("ImageIndex",index);
-        startActivity(intent);
-
+    private void setImage(String index)
+    {
+        ImageView imageView=findViewById(R.id.img1);
+        switch(index){
+            case "1":
+                imageView.setImageResource(R.drawable.fazal);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.sura);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.ansar);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.nihal);
+                break;
+        }
     }
 }
